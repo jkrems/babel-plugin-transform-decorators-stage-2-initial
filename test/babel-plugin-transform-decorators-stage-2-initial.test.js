65b7d6cd6a731290c39b09eab0a15cfb66cf9c33
Jan Krems <jan.krems@gmail.com>
2016-07-31 11:14:10 -0700

    test: Double-decorated property throws

--- test/babel-plugin-transform-decorators-stage-2-initial.test.js
+++ test/babel-plugin-transform-decorators-stage-2-initial.test.js
@@ -10,27 +10,41 @@ var babelPluginTransformDecoratorsStage2Initial = require('../');
 var TEST_DIR = __dirname + '/language/decorators';
 var TEST_CASES = fs.readdirSync(TEST_DIR);
 
+function toES6Code(source) {
+  return babel.transform(source, {
+    plugins: [
+      babelPluginTransformDecoratorsStage2Initial
+    ]
+  }).code;
+}
+
 describe('apply decorators', function () {
   TEST_CASES.forEach(function (filename) {
     var fullPath = TEST_DIR + '/' + filename;
 
     describe(filename, function () {
-      var result;
+      var es6Code;
       before('compile', function () {
         var source = fs.readFileSync(fullPath, 'utf8');
-        result = babel.transform(source, {
-          plugins: [
-            babelPluginTransformDecoratorsStage2Initial
-          ]
-        });
+        es6Code = toES6Code(source);
       });
 
       it('runs successfully', function () {
-        vm.runInNewContext('"use strict";\n' + result.code, {
+        vm.runInNewContext('"use strict";\n' + es6Code, {
           assert: assert,
           console: console
         }, { filename: fullPath });
       });
+    });
+  });
+
+  describe('getter/setter both decorated', function () {
+    it('triggers an error', function () {
+      var err = assert.throws(function () {
+        toES6Code('class X { @d get a() {} @d set a(v) {} }');
+      });
+      assert.hasType('Contains code snippet', String, err.codeFrame);
+      assert.include('Cannot decorate both getter and setter for the same property', err.message);
     });
   });
 });
