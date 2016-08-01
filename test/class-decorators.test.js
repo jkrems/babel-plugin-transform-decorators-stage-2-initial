d2d7c1df07ad518c953994a7ac8a0d7d8ba8776a
Jan Krems <jan.krems@gmail.com>
2016-08-01 08:49:50 -0700

    test: Create class decorator test setup

--- test/class-decorators.test.js
+++ test/class-decorators.test.js
@@ -7,18 +7,19 @@ var babel = require('babel-core');
 
 var babelPluginTransformDecoratorsStage2Initial = require('../');
 
-var TEST_DIR = __dirname + '/language/decorators';
+var TEST_DIR = __dirname + '/language/class-decorators';
 var TEST_CASES = fs.readdirSync(TEST_DIR);
 
 function toES6Code(source) {
   return babel.transform(source, {
     plugins: [
-      babelPluginTransformDecoratorsStage2Initial
+      babelPluginTransformDecoratorsStage2Initial,
+      'transform-es2015-classes'
     ]
   }).code;
 }
 
-describe('apply decorators', function () {
+describe('class-decorators', function () {
   TEST_CASES.forEach(function (filename) {
     var fullPath = TEST_DIR + '/' + filename;
 
